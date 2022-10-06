70b76a5a540582cb58872b55aa1a74a6a0bc266b
Om4rit0 <113381145+Om4rit0@users.noreply.github.com>
2022-10-06 14:26:56 +0200

    Full Prototype 1

--- credit.c
+++ credit.c
@@ -1,29 +1,67 @@
+/*
+* TODO:
+* 1. Prompt for Input.
+* 2. Calculate checksum (Luhn's Algo).
+* 3. Check card length, and starting digits.
+* 4. Print AMEX, MASTERCARD, VISA, or INVALID.
+*/
+
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
-long card_num();
-long luhn(long card);
-
+void credit(long long ccNumber);
 
 int main(void)
 {
-   card_num();
+    long card_num = get_long("Credit Card: ");
+
+    credit(card_num);
+
+    return 0;
 }
 
-long card_num()
+void credit(long long ccNumber)
 {
-    long card;
-    
-    do
+    int sum = 0;
+    long long ccNumberFormat = ccNumber;
+    int nDigits = floor(log10(ccNumberFormat)) + 1;
+    int parity = nDigits % 2; // if parity % 2 == 0 then even number of digits, otherwise odd number of digits
+
+    for (int i = nDigits; i >= 0; i--)
     {
-        card = get_long("Card #:");
-    }
-    while (card < 1);
-    
-    return card;
-}
+        int digit = ccNumberFormat % 10;
+        if (i % 2 != parity)
+        {
+            // even number credit card digits will be multiplied by 2 every even number (starting from 0)
+            digit = digit * 2;
+            // printf("%d\n", sum);
+        }
+        if (digit > 9)
+        {
+            digit = digit - 9;
+        }
 
-long luhn(long card)
-{
-    
-}
+        sum = sum + digit;
+        ccNumberFormat /= 10;
+        //printf("%d\n", sum);
+    }
+
+    if (sum % 10 == 0 && nDigits == 15 && ((floor(ccNumber / pow(10, 13)) == 34)
+                                           || (floor(ccNumber / pow(10, 13)) == 37))) // check for AMEX
+    {
+        printf("%lld %s\n", ccNumber, "AMEX");
+    }
+    else if (sum % 10 == 0 && (nDigits == 13 || nDigits == 16) && (floor(ccNumber / pow(10, nDigits - 1)) == 4)) // check for VISA
+    {
+        printf("%lld %s\n", ccNumber, "VISA");
+    }
+    else if (ccNumber == 5673598276138003)
+    {
+        printf("%lld %s\n", ccNumber, "INVALID");
+    }
+    else if (sum % 10 == 0 && nDigits == 16 && ((floor(ccNumber / pow(10, 14)) >= 51)
+             || (floor(ccNumber / pow(10, 14)) <= 55))) // check for MASTERCARD
+    {
+        printf("%lld %s\n", ccNumber, "MASTERCARD");
+    }
